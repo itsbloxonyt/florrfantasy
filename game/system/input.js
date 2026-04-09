97d8de3dbb2440223d99d5eb586fa9ebe854c7d2
itsbloxonyt <phamhoanglam7749@gmail.com>
2026-04-09 17:29:00 +0700

    four

--- game/system/input.js
+++ game/system/input.js
@@ -3,8 +3,8 @@ import {
   activeSlots, swapSlots, petals, petalStateCache, slotCooldown,
   setSwapCooldown, setLastSwapTime, lastSwapTime,
   player,
-} from './state.js';
-import { rebuildPetals } from './petals.js';
+} from './game/state.js';
+import { rebuildPetals } from './content/petals.js';
 import { buildPetalBarUI, attachSlotEvents } from './ui.js';
 
 export const keys = {};
