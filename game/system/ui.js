7e05cfdfffaf4720013711f8c60643645301b832
itsbloxonyt <phamhoanglam7749@gmail.com>
2026-04-09 21:39:41 +0700

    asdsadsdsdadsadsdasadsadads

--- game/system/ui.js
+++ game/system/ui.js
@@ -2,8 +2,8 @@ import {
   ACTIVE_COUNT, SWAP_COUNT, SWAP_CD_MAX,
   PETAL_TYPES, activeSlots, swapSlots, petals, petalStateCache,
   player, swapCooldown, setSwapCooldown,
-} from './game/state.js';
-import { rebuildPetals } from './game/content/petals.js';
+} from '../state.js';
+import { rebuildPetals } from '../content/petals.js';
 
 let dragSrc = null;
 
